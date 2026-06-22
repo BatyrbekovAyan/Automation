47230d8376cecebb64df1e52cf4ec2d0510435f7
Ayan <ayanbatyrbekov@gmail.com>
2026-06-22 20:41:49 +0500

    feat(chat): add isDeleted to ChatDialog model

--- Assets/Scripts/Chat/ChatDialog.cs
+++ Assets/Scripts/Chat/ChatDialog.cs
@@ -10,6 +10,7 @@ public class ChatDialog
     public string last_message_data;
     public string last_timestamp;
     public bool isArchived;
+    public bool isDeleted;
     public int unread_count;
     public string last_message_id;
     public string last_message_type;
